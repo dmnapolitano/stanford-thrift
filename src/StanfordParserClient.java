198dfe5395ccd7276705cd52bed738f3d74daa3a
Diane M. Napolitano <dnapolitano@ets.org>
2013-02-28 15:56:31 -0500

    Pulling in changes from master branch.

---
+++ src/StanfordParserClient.java
@@ -0,0 +1,74 @@
+// Generated code
+import parser.*;
+
+import java.io.InputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.List;
+
+
+import org.apache.thrift.TException;
+import org.apache.thrift.transport.TSSLTransportFactory;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TSSLTransportFactory.TSSLTransportParameters;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+
+public class StanfordParserClient {
+
+    public static void main(String [] args) {
+
+        String server = "";
+        Integer port = 0;
+        String inputFilename = "";
+
+        if (args.length == 3) {
+            server = args[0];
+            port = Integer.parseInt(args[1]);
+            inputFilename = args[2];
+        }
+        else {
+            System.err.println("Usage: StanfordParserClient <server> <port> <inputfile>");
+            System.exit(2);
+        }
+
+        try {
+            TTransport transport;
+            transport = new TSocket(server, port);
+            transport.open();
+
+            TProtocol protocol = new  TBinaryProtocol(transport);
+            StanfordParser.Client client = new StanfordParser.Client(protocol);
+
+            perform(client, inputFilename);
+
+            transport.close();
+        } catch (TException x) {
+            x.printStackTrace();
+        }
+    }
+
+    private static void perform(StanfordParser.Client client, String inputFilename) throws TException
+    {
+        FileReader infile = null;
+
+        try {
+            infile = new FileReader(inputFilename);
+            BufferedReader in = new BufferedReader(infile);
+            while (in.ready()) {
+                String sentence = in.readLine();
+                List<ParseTree> trees = client.parse_text(sentence, null);
+                for (ParseTree tree : trees)
+                {
+                    System.out.println(tree.tree);
+                }
+            }
+            in.close();
+        }
+        catch (IOException e) {
+          e.printStackTrace();
+        }
+    }
+}
