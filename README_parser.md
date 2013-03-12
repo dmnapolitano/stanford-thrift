7426046d13fa130b44d83e5b2221e48c6442f8e1
Diane M. Napolitano <dnapolitano@ets.org>
2013-03-12 15:10:00 -0400

    Changes from master

---
+++ README_parser.md
@@ -0,0 +1,50 @@
+How to Get Parse Trees from the Stanford Parser via this Apache Thrift Server
+=============================================================================
+
+## How to Interact with the Methods and Data Structures
+
+The core return type here is a data structure called `ParseTree` which has two members:
+
+* `tree`: A string representing your parse tree (or, quite optionally, parse treeS; keep reading).
+* `score`: A double representing the score for that parse.
+
+In order to get these `ParseTree` objects, you have two choices, depending on whether or not you'd like Stanford's tokenizer to do some of the work for you.  The arguments are supplied in both Python and Java terms for ease of understanding, but again, see the clients if you're confused.  Keep reading for more information on the `outputFormat` parameter to each of these methods.
+
+* `parse_text(text, outputFormat)` where `text` is a Java `String`/Python `str` or `unicode`, `outputFormat` is a Java `List<String>`/Python list containing `str`/`unicode`.
+  Returns: Java `List<ParseTree>`/Python list containing `ParseTree` objects.
+  Given any untokenized, arbitrary text, use Stanford's sentence and word tokenizers to do that bit of the work.
+
+* `parse_tokens(tokens, outputFormat)` where `tokens` is a Java `List<String>`/Python list containing `str`/`unicode`, `outputFormat` is a Java `List<String>`/Python list containing `str`/`unicode`.
+   Returns: A `ParseTree` object.
+   Given a single sentence worth of output from toksent and expunct (for example), return that sentence's corresponding result from Stanford Parser.  Does not use Stanford's tokenizers.
+   
+##### What one can do with the `outputFormat` argument to both of these methods
+
+The purpose of the `outputFormat` argument is to allow one to supply arguments in the same style as one would via command-line call to the Stanford Parser. **The only command-line switches supported here are `-outputFormat` and `-outputFormatOptions`, but they are supported in full.**  By that I mean any valid argument to each of those options is also valid here.
+You can also pass in `null`/`None` and that will return parse trees in this server's default format of `-outputFormat "oneline"`.
+You can also supply multiple `-outputFormat` arguments, but note: you'll get back all of those parse trees, but altogether in the `tree` member of the returned `ParseTree` object, separated by two newlines (`\n\n`).
+Thus, a call to a client object `client` that looks like:
+
+```python
+result = client.parse_tokens(["The", "cat", "sat", "on", "the", "mat", "."], 
+                             ["-outputFormat", "typedDependencies,penn", "-outputFormatOptions", "basicDependencies"])
+```
+
+will have the following inside the `tree` member:
+
+```Python
+(ROOT
+  (S
+    (NP (DT The) (NN cat))
+    (VP (VBD sat)
+      (PP (IN on)
+        (NP (DT the) (NN mat))))
+    (. .)))
+
+det(cat-2, The-1)
+nsubj(sat-3, cat-2)
+root(ROOT-0, sat-3)
+prep(sat-3, on-4)
+det(mat-6, the-5)
+pobj(on-4, mat-6)
+```
--- scripts/parser_client_with_expunct.py
+++ scripts/parser_client_with_expunct.py
@@ -4,8 +4,8 @@
 # It is also for me to unit test things. >:)
 # For an alternative way to call the parser (with Python), please see parser_client.py.
 
-from stanfordparser import StanfordParser
-from stanfordparser.ttypes import *
+from corenlp import StanfordCoreNLP
+from corenlp.ttypes import *
 from thrift import Thrift
 from thrift.transport import TSocket, TTransport
 from thrift.protocol import TBinaryProtocol
@@ -30,13 +30,15 @@ else:
 #sentences = ["Well aware that the opinions and belief of men depend not on their own will , but follow involuntarily the evidence proposed to their minds ; that Almighty God hath created the mind free , and manifested his supreme will that free it shall remain by making it altogether insusceptible of restraint ; that all attempts to influence it by temporal punishments , or burthens , or by civil incapacitations , tend only to beget habits of hypocrisy and meanness , and are a departure from the plan of the holy author of our religion , who being lord both of body and mind , yet chose not to propagate it by coercions on either , as was in his almighty power to do , but to exalt it by its influence on reason alone ; that the impious presumption of legislature and ruler , civil as well as ecclesiastical , who , being themselves but fallible and uninspired men , have assumed dominion over the faith of others , setting up their own opinions and modes of thinking as the only true and infallible , and as such endeavoring to impose them on others , hath established and maintained false religions over the greatest part of the world and through all time : that to compel a man to furnish contributions of money for the propagation of opinions which he disbelieves and abhors is sinful and tyrannical ; that even the forcing him to support this or that teacher of his own religious persuasion is depriving him of the comfortable liberty of giving his contributions to the particular pastor whose morals he would make his pattern and whose powers he feels most persuasive to righteousness , and is withdrawing from the ministry those temporary rewards which , proceeding from an approbation of their personal conduct , are an additional incitement to earnest and unremitting labors for the instruction of mankind ; that our civil rights have no dependence on our religious opinions , any more than our opinions in physics or geometry ; and therefore the proscribing any citizen as unworthy the public confidence by laying upon him an incapacity of being called to offices of trust or emolument , unless he profess or renounce this or that religious opinion , is depriving him injudiciously of those privileges and advantages to which , in common with his fellow citizens , he has a natural right ; that it tends also to corrupt the principles of that very religion it is meant to encourage , by bribing with a monopoly of worldly honors and emoluments those who will externally profess and conform to it ; that though indeed these are criminals who do not withstand such temptation , yet neither are those innocent who lay the bait in their way ; that the opinions of men are not the object of civil government , nor under its jurisdiction ; that to suffer the civil magistrate to intrude his powers into the field of opinion and to restrain the profession or propagation of principles on supposition of their ill tendency is a dangerous fallacy , which at once destroys all religious liberty , because he being of course judge of that tendency will make his opinions the rule of judgment and approve or condemn the sentiments of others only as they shall square with or suffer from his own ; that it is time enough for the rightful purposes of civil government for its officers to interfere when principles break out into overt acts against peace and good order ; and finally , that the truth is great and will prevail if left to herself ; that she is the proper and sufficient antagonist to error , and has nothing to fear from the conflict unless by human interposition disarmed of her natural weapons , free argument and debate ; errors ceasing to be dangerous when it is permitted freely to contradict them ."]
 
 # Good for testing what is probably the most common use-case.  Taken from the English Wikipedia entry for "Fox". :)
-sentences = ["Members of about 37 species are referred to as foxes, of which only 12 species actually belong to the Vulpes genus of \"true foxes\".", "By far the most common and widespread species of fox is the red fox (Vulpes vulpes), although various species are found on almost every continent.", "The presence of fox-like carnivores all over the globe, together with their widespread reputation for cunning, has contributed to their appearance in popular culture and folklore in many societies around the world (see also Foxes in culture).", "The hunting of foxes with packs of hounds, long an established pursuit in Europe, especially the British Isles, was exported by European settlers to various parts of the New World."]
+#sentences = ["Members of about 37 species are referred to as foxes, of which only 12 species actually belong to the Vulpes genus of \"true foxes\".", "By far the most common and widespread species of fox is the red fox (Vulpes vulpes), although various species are found on almost every continent.", "The presence of fox-like carnivores all over the globe, together with their widespread reputation for cunning, has contributed to their appearance in popular culture and folklore in many societies around the world (see also Foxes in culture).", "The hunting of foxes with packs of hounds, long an established pursuit in Europe, especially the British Isles, was exported by European settlers to various parts of the New World."]
 
+# for testing named entity systems
+sentences = ["My name is Diane and I live in New Jersey.",  "I sometimes go to New York.",  "The Food and Drug Administration is an organization."]
 
 transport = TSocket.TSocket(server, port)
 transport = TTransport.TBufferedTransport(transport)
 protocol = TBinaryProtocol.TBinaryProtocol(transport)
-client = StanfordParser.Client(protocol)
+client = StanfordCoreNLP.Client(protocol)
 
 transport.open()
 
@@ -44,7 +46,7 @@ transport.open()
 # Note that the DEFAULT is what you would get if you specified "oneline" on the command line, or "None" here.
 #outputOptions = ["-outputFormat", "typedDependencies,penn", "-outputFormatOptions", "basicDependencies"]
 #outputOptions = None
-outputOptions = ["-outputFormat", "wordsAndTags"]
+outputOptions = ["-outputFormat", "wordsAndTags,oneline"]
 
 for sentence in sentences:
     #sentence = UnicodeDammit(sentence, ["windows-1252", "utf8"]).unicode_markup
