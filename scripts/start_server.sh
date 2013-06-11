b2ee26b56f693088a87a156def71fee45b818aa6
Diane M. Napolitano <dnapolitano@ets.org>
2013-06-11 13:32:49 -0400

    Pulling in changes from master branch

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,19 +1,18 @@
 #!/bin/zsh
 
 MAINDIR=$(dirname $0:A)/../
-CMD="java -cp $CLASSPATH:$MAINDIR/stanford-corenlp-wrapper.jar -Xmx4096m -XX:-UseGCOverheadLimit StanfordCoreNLPServer"
 
-#e.g., start_server.sh  9999
-#      start_server.sh  9999 edu/stanford/nlp/models/lexparser/englishFactored.ser.gz
+#e.g., start_server.sh  9999 edu/stanford/nlp/models/lexparser/englishFactored.ser.gz 4096m
 
 
-if [ $# -eq 2 ]; then
+if [ $# -eq 3 ]; then
  PORT=$1
  MODEL=$2
- eval $CMD $PORT $MODEL
-elif [ $# -eq 1 ]; then
- PORT=$1
- eval $CMD $PORT
+ HEAPSIZE=$3
+ java -cp $CLASSPATH:$MAINDIR/stanford-corenlp-wrapper.jar -Xmx$HEAPSIZE -XX:-UseGCOverheadLimit StanfordCoreNLPServer $PORT $MODEL
 else
- echo "Usage: start_server.sh <port> (<model>)"
+ echo "Usage: $(basename $0) <port> <model> <heapsize>"
+ echo "e.g., $(basename $0) 9999 edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz 4G"
 fi
+
+
