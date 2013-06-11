fef5c5abc5cf5c0825fb7e68fde8500eccee45c4
Michael Heilman <mheilman@ets.org>
2013-06-11 11:40:34 -0400

    modified scripts/start_server.sh to use zsh and work from any directory

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,19 @@
-#!/bin/bash
+#!/bin/zsh
 
-CMD="java -cp $CLASSPATH:stanford-corenlp-wrapper.jar -Xmx4096m -XX:-UseGCOverheadLimit StanfordCoreNLPServer"
+MAINDIR=$(dirname $0:A)/../
+CMD="java -cp $CLASSPATH:$MAINDIR/stanford-corenlp-wrapper.jar -Xmx4096m -XX:-UseGCOverheadLimit StanfordCoreNLPServer"
 
-#if [ $# -eq 1 ]; then
-PORT=$1
-MODEL=$2
-$CMD $PORT $MODEL
-#else
-#    echo "Usage: start_server.sh <port>"
-#fi
+#e.g., start_server.sh  9999
+#      start_server.sh  9999 edu/stanford/nlp/models/lexparser/englishFactored.ser.gz
+
+
+if [ $# -eq 2 ]; then
+ PORT=$1
+ MODEL=$2
+ eval $CMD $PORT $MODEL
+elif [ $# -eq 1 ]; then
+ PORT=$1
+ eval $CMD $PORT
+else
+ echo "Usage: start_server.sh <port> (<model>)"
+fi
