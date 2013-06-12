28152c9987a0c4e6c859cbfb4b95b82746c92b4b
Diane M. Napolitano <dnapolitano@ets.org>
2013-06-12 15:46:17 -0400

    Made parser model an optional argument in start_server.sh again.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,17 +2,20 @@
 
 MAINDIR=$(dirname $0:A)/../
 
-#e.g., start_server.sh  9999 edu/stanford/nlp/models/lexparser/englishFactored.ser.gz 4096m
-
-
 if [ $# -eq 3 ]; then
  PORT=$1
  MODEL=$2
  HEAPSIZE=$3
  java -cp $CLASSPATH:$MAINDIR/stanford-corenlp-wrapper.jar -Xmx$HEAPSIZE -XX:-UseGCOverheadLimit StanfordCoreNLPServer $PORT $MODEL
+elif [ $# -eq 2 ]; then
+ PORT=$1
+ HEAPSIZE=$2
+ java -cp $CLASSPATH:$MAINDIR/stanford-corenlp-wrapper.jar -Xmx$HEAPSIZE -XX:-UseGCOverheadLimit StanfordCoreNLPServer $PORT
 else
- echo "Usage: $(basename $0) <port> <model> <heapsize>"
- echo "e.g., $(basename $0) 9999 edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz 4G"
+ echo "Usage: $(basename $0) <port> [<model>] <heapsize>"
+ echo "e.g., $(basename $0) 9999 edu/stanford/nlp/models/lexparser/englishFactored.ser.gz 4G"
+ echo "or, $(basename $0) 9999 4G"
+ echo "Parser model is optional; will use edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz by default."
 fi
 
 
