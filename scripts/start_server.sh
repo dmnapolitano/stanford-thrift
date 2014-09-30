69407ce564db665a4f5bb966144c99f74bb5dab0
Napolitano, Diane <dnapolitano@ets.org>
2014-09-30 13:26:26 -0400

    Updated start_server.sh to use new package path.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,7 +6,7 @@ if [ $# -eq 3 ]; then
  PORT=$1
  HEAPSIZE=$2
  CONFIG=$3
- java -cp $CLASSPATH:$MAINDIR/stanford-corenlp-wrapper.jar -Xmx$HEAPSIZE -XX:-UseGCOverheadLimit StanfordCoreNLPServer $PORT $CONFIG
+ java -cp $CLASSPATH:$MAINDIR/stanford-corenlp-wrapper.jar -Xmx$HEAPSIZE -XX:-UseGCOverheadLimit org.ets.research.nlp.stanford_thrift.StanfordCoreNLPServer $PORT $CONFIG
 else
  echo "Usage: $(basename $0) <port> <heapsize> <config file>"
  echo "See scripts/ for an example config file."
